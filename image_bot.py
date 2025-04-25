a05d36f8e40d06e356cdadbfd48af1049da0a22a
jake7869 <jakealfiebradley@gmail.com>
2025-04-25 01:31:30 +0100

    Add files via upload

---
+++ image_bot.py
@@ -0,0 +1,15 @@
+
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot connected as {bot.user}")
+
+bot.run(os.getenv("YOUR_BOT_TOKEN"))
